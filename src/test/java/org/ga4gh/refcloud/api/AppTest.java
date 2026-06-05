b438ee9ea40e3ddeba410841e653ae633b290c97
Jeremy Adams <jeremy.adams@ga4gh.org>
2026-06-05 11:56:43 -0400

    reinitialize test directory

---
+++ src/test/java/org/ga4gh/refcloud/api/AppTest.java
@@ -0,0 +1,12 @@
+package org.ga4gh.refcloud.api;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+    
+    @Test
+    public void initialTest() {
+        assertTrue(true, "this test should always pass");
+    }
+}
