a8ce2dee5f49e59642bf8b00d8e2378c9ba524d8
Jeremy Adams <jeremy.adams@ga4gh.org>
2026-06-15 10:30:33 -0400

    secure DRS Object endpoint - not fully implemented

--- src/main/java/org/ga4gh/refcloud/api/security/SecurityConfig.java
+++ src/main/java/org/ga4gh/refcloud/api/security/SecurityConfig.java
@@ -2,8 +2,11 @@ package org.ga4gh.refcloud.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+<<<<<<< HEAD
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+=======
+>>>>>>> aac3d10 (secure DRS Object endpoint - not fully implemented)
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
